934e248b0a2d7ed614ce0659c507586317949f1d
Melvin S Alex <120078416+melvinshaji22@users.noreply.github.com>
2024-08-07 16:20:50 +0530

    feat: created tabs (#3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,84 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { FormlyFieldConfig, FormlyFormOptions,FormlyModule } from '@ngx-formly/core';
+import { FormlyFieldTabs } from './tabs.type';
+import { FormlyMaterialModule } from '@ngx-formly/material';;
+
+
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone:true,
+  imports:[RouterOutlet,FormlyFieldTabs,ReactiveFormsModule,FormlyMaterialModule,FormlyModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'VO-formly-POC';
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+
+  fields: FormlyFieldConfig[] = [
+    {
+      type: 'tabs',
+      fieldGroup: [
+        {
+          props: { label: 'Personal data' },
+          fieldGroup: [
+            {
+              key: 'firstname',
+              type: 'input',
+              props: {
+                label: 'First name',
+                required: true,
+              },
+            },
+            {
+              key: 'age',
+              type: 'input',
+              props: {
+                type: 'number',
+                label: 'Age',
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          props: { label: 'Destination' },
+          fieldGroup: [
+            {
+              key: 'country',
+              type: 'input',
+              props: {
+                label: 'Country',
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          props: { label: 'Day of the trip' },
+          fieldGroup: [
+            {
+              key: 'day',
+              type: 'input',
+              props: {
+                type: 'date',
+                label: 'Day of the trip',
+                required: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  submit() {
+    alert(JSON.stringify(this.model));
+  }
 }
