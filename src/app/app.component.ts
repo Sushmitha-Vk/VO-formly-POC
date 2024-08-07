6626db7ee53a2a3635d8a6051497551bd2b25f20
madhumitha13579 <madhumitha.p@muraai.com>
2024-08-07 19:05:37 +0530

    select and radio btn fields added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,16 +48,135 @@ export class AppComponent {
           ],
         },
         {
-          props: { label: 'Destination' },
+          props: { label: 'ESG/EH' },
           fieldGroup: [
             {
-              key: 'country',
-              type: 'input',
+              key: 'countryOfHeadQuarter',
+              type: 'select',
               props: {
-                label: 'Country',
+                label: 'Country of Headquarter',
+                required: true,
+                options: [
+                  { value: 'India', label: 'India' },
+                  { value: 'China', label: 'China' },
+                  { value: 'Japan', label: 'Japan' },
+                  { value: 'France', label: 'France'},
+                  { value: 'Germany', label: 'Germany'},
+                  { value: 'Italy', label: 'Italy'},
+                ],
+              },
+            },
+            {
+              key: 'noOfEmployees',
+              type: 'number',
+              props: {
+                label: 'Number of Employees',
                 required: true,
               },
             },
+            {
+              key: 'revenue',
+              type: 'input',
+              props: {
+                label: 'Revenue of company',
+                required: true,
+              },
+            },
+            {
+              key: 'doesYourCompUseRenewableElectricity',
+              type: 'radio',
+              props: {
+                label: 'Does your company use renewable electricity?',
+                required: true,
+                options: [
+                  { value: 'Yes', label: 'Yes' },
+                  { value: 'No', label: 'No' }
+                ],
+              },
+            },
+            {
+              key: 'ghgEmissionInventoryAssurance',
+              type: 'radio',
+              props: {
+                label: 'Has your company has done GHG Emission inventory assurance by 3rd party?',
+                required: true,
+                options: [
+                  { value: 'Yes', label: 'Yes' },
+                  { value: 'No', label: 'No' }
+                ],
+              },
+            },
+            {
+              key: 'doesYourCompanyCalculateTheGhgEmissions',
+              type: 'radio',
+              props: {
+                label: 'Does your company calculate the GHG Emissions?',
+                required: true,
+                options: [
+                  { value: 'Yes', label: 'Yes' },
+                  { value: 'No', label: 'No' }
+                ],
+              },
+            },
+            {
+              key: 'scope1Emissions',
+              type: 'input',
+              props: {
+                label: 'Scope 1 emissions',
+                placeholder: 'Provide GHG Emission data tCO2e',
+                required: true,
+              },
+              expressions: {
+                hide: "model.doesYourCompanyCalculateTheGhgEmissions !== 'Yes'",
+              },
+            },
+            {
+              key: 'scope2Emissions',
+              type: 'input',
+              props: {
+                label: 'Scope 2 emissions',
+                placeholder: 'Provide GHG Emission data tCO2e',
+                required: true,
+              },
+              expressions: {
+                hide: "model.doesYourCompanyCalculateTheGhgEmissions !== 'Yes'",
+              },
+            },
+            {
+              key: 'scope3Emissions',
+              type: 'input',
+              props: {
+                label: 'Scope 3 emissions',
+                placeholder: 'Provide GHG Emission data tCO2e',
+                required: true,
+              },
+              expressions: {
+                hide: "model.doesYourCompanyCalculateTheGhgEmissions !== 'Yes'",
+              },
+            },
+            {
+              key: 'reasonForNotCalculationgGhgEmissions',
+              type: 'input',
+              props: {
+                label: 'Please provide reasons for not calculating the GHG emissions',
+                required: true,
+              },
+              expressions: {
+                hide: "model.doesYourCompanyCalculateTheGhgEmissions !== 'No'",
+              },
+            },
+            {
+              key: 'yourGhgEmissionsDataArePublished',
+              type: 'radio',
+              props: {
+                label: 'Your GHG emissions data are published?',
+                required: true,
+                options: [
+                  { value: 'Yes', label: 'Yes' },
+                  { value: 'No', label: 'No' }
+                ],
+              },
+            }
           ],
         },
         {
