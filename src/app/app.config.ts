6bdb7f0d68d45074e629332daa4d29e6650b096c
Sushmitha V <sushmitha.v@muraai.com>
2024-08-07 16:58:10 +0530

    Added expansion panel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, input, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,13 +6,16 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyFieldTabs } from './tabs.type';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormlyFieldExpansionPanel } from './expansion-panel.types';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
     provideAnimations(),
     importProvidersFrom(FormlyModule.forRoot({
       validationMessages: [{ name: 'required', message: 'This field is required' }],
-      types: [{ name: 'tabs', component: FormlyFieldTabs }],
+      types: [
+        { name: 'tabs', component: FormlyFieldTabs },
+        { name: 'exp-panel', component: FormlyFieldExpansionPanel },],
     }),)
    
   ]
